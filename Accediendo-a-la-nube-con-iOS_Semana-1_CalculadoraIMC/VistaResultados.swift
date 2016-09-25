cf4eb581474195d754847ebc5f505bd69f9dd02c
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-09-25 18:31:19 -0500

    Clase "Creación de app multipantalla".

---
+++ Accediendo-a-la-nube-con-iOS_Semana-1_CalculadoraIMC/VistaResultados.swift
@@ -0,0 +1,36 @@
+//
+//  VistaResultados.swift
+//  Accediendo-a-la-nube-con-iOS_Semana-1_CalculadoraIMC
+//
+//  Created by Juan Carlos Carbajal Ipenza on 25/09/16.
+//  Copyright © 2016 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import UIKit
+
+class VistaResultados: UIViewController {
+    @IBOutlet weak var resultadoIMC: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
